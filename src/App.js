8e454c43c95b57de686abfdd88fe9f7db0ba8bfe
AnCial <aguinaldo.marcial1@gmail.com>
2024-07-23 13:52:31 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+const initialItems = [
+  { id: 1, name: "Toothbrush", quantity: 5, packed: false },
+  { id: 2, name: "Underwear", quantity: 5, packed: false },
+  { id: 3, name: "Phone Charger", quantity: 5, packed: false },
+  { id: 4, name: "Socks", quantity: 5, packed: true },
+  { id: 5, name: "Shirts", quantity: 5, packed: false },
+  { id: 6, name: "Pants", quantity: 5, packed: false },
+  { id: 7, name: "Sandwich", quantity: 5, packed: true },
+  { id: 8, name: "Water", quantity: 5, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -13,19 +24,57 @@ function Logo() {
   return <h1>👜 Pack Me! 🧳</h1>;
 }
 function Form() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const { quantity, item } = form.elements;
+    const newItem = {
+      id: initialItems.length + 1,
+      name: item.value,
+      quantity: quantity.value,
+      packed: false,
+    };
+    initialItems.push(newItem);
+    form.reset();
+  }
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>Don't forget to me Pack me! 😘</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
   return (
     <div className="list">
-      <h3>My Packing List</h3>
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
     </div>
   );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.name}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
     <footer className="stats">
